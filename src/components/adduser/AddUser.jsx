0217c2fa146c5fab2cb8f54b81b37fc436191650
wakai-megumi <tarun483sharma@gmail.com>
2023-06-16 09:56:14 +0530

    edit add user

--- src/components/adduser/AddUser.jsx
+++ src/components/adduser/AddUser.jsx
@@ -96,124 +96,125 @@ const AddUser = () => {
                 }
             }
         };
-        //image take too long so we can cancel the image upload
-        const cancelImageUpload = () => {
-            if (uploadRequest) {
-                uploadRequest.cancel('Image upload canceled by user');
-            }
-        };
-
-        return (
-            <form className="add-user-container" onSubmit={handleSubmit}>
-                <div className="form-column">
-                    <div className="input-section">
-                        <label htmlFor="email">Email</label>
-                        <input
-                            type="text"
-                            id="email"
-                            name="email"
-                            value={formData.email}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="input-section">
-                        <label htmlFor="password">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            name="password"
-                            value={formData.password}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="input-section">
-                        <label htmlFor="country">Country</label>
-                        <input
-                            type="text"
-                            id="country"
-                            name="country"
-                            value={formData.country}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="input-section">
-                        <label htmlFor="city">City</label>
-                        <input
-                            type="text"
-                            id="city"
-                            name="city"
-                            value={formData.city}
-                            onChange={handleChange}
-                        />
-                    </div>
-                </div>
-
-                <div className="form-column">
-                    <div className="input-section">
-                        <label htmlFor="username">Username</label>
-                        <input
-                            type="text"
-                            id="username"
-                            name="username"
-                            value={formData.username}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="input-section">
-                        <label htmlFor="phone">Phone</label>
-                        <input
-                            type="text"
-                            id="phone"
-                            name="phone"
-                            value={formData.phone}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="input-section">
-                        <label htmlFor="address">Address</label>
-                        <input
-                            type="text"
-                            id="address"
-                            name="address"
-                            value={formData.address}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="input-section">
-                        <label htmlFor="isAdmin">Admin</label>
-                        <input
-                            type="checkbox"
-                            id="isAdmin"
-                            name="isAdmin"
-                            checked={formData.isAdmin}
-                            onChange={handleChange}
-                        />
-                    </div>
-                </div>
-                <div className="image-column">
-                    {formData.profileimage ? (
-                        <img src={formData.profileimage} alt="Uploaded" className="user-image" />
-                    ) : (
-                        <div className="placeholder">Image Placeholder</div>
-                    )}
-                    <div className="upload-section">
-                        <label htmlFor="image-upload" className="upload-icon">
-                            <FiUpload />
-                        </label>
-                        <input
-                            type="file"
-                            id="image-upload"
-                            accept="image/*"
-                            onChange={handleImageUpload}
-                        />
-                    </div>
-                    <button type="button" onClick={cancelImageUpload}>Cancel Upload</button>
-                </div>
-                <button type="submit" disabled={loading}>
-                    Submit
-                </button>
-            </form>
-        );
+    }
+    //image take too long so we can cancel the image upload
+    const cancelImageUpload = () => {
+        if (uploadRequest) {
+            uploadRequest.cancel('Image upload canceled by user');
+        }
     };
+    return (
+        <form className="add-user-container" onSubmit={handleSubmit}>
+            <div className="form-column">
+                <div className="input-section">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="input-section">
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="input-section">
+                    <label htmlFor="country">Country</label>
+                    <input
+                        type="text"
+                        id="country"
+                        name="country"
+                        value={formData.country}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="input-section">
+                    <label htmlFor="city">City</label>
+                    <input
+                        type="text"
+                        id="city"
+                        name="city"
+                        value={formData.city}
+                        onChange={handleChange}
+                    />
+                </div>
+            </div>
 
-    export default AddUser;
+            <div className="form-column">
+                <div className="input-section">
+                    <label htmlFor="username">Username</label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="input-section">
+                    <label htmlFor="phone">Phone</label>
+                    <input
+                        type="text"
+                        id="phone"
+                        name="phone"
+                        value={formData.phone}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="input-section">
+                    <label htmlFor="address">Address</label>
+                    <input
+                        type="text"
+                        id="address"
+                        name="address"
+                        value={formData.address}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div className="input-section">
+                    <label htmlFor="isAdmin">Admin</label>
+                    <input
+                        type="checkbox"
+                        id="isAdmin"
+                        name="isAdmin"
+                        checked={formData.isAdmin}
+                        onChange={handleChange}
+                    />
+                </div>
+            </div>
+            <div className="image-column">
+                {formData.profileimage ? (
+                    <img src={formData.profileimage} alt="Uploaded" className="user-image" />
+                ) : (
+                    <div className="placeholder">Image Placeholder</div>
+                )}
+                <div className="upload-section">
+                    <label htmlFor="image-upload" className="upload-icon">
+                        <FiUpload />
+                    </label>
+                    <input
+                        type="file"
+                        id="image-upload"
+                        accept="image/*"
+                        onChange={handleImageUpload}
+                    />
+                </div>
+                <button type="button" onClick={cancelImageUpload}>Cancel Upload</button>
+            </div>
+            <button type="submit" disabled={loading}>
+                Submit
+            </button>
+        </form>
+    );
+
+
+}
+export default AddUser;
